8b387888888582fd1d5c9d6d9ba90da8fc46f0ed
Jonas Sulzer <jonas@violoncello.ch>
2018-11-16 16:32:53 +0100

    add license
    Signed-off-by: Jonas Sulzer <jonas@violoncello.ch>

--- views/build/titleimage-position.php
+++ views/build/titleimage-position.php
@@ -1,3 +1,24 @@
+/**
+ * @copyright Copyright (c) 2018, violoncello.ch
+ *
+ * @author Jonas Sulzer <jonas@violoncello.ch>
+ *
+ * @license AGPL-3.0
+ *
+ * This code is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License, version 3,
+ * as published by the Free Software Foundation.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License, version 3,
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>
+ *
+ */
+ 
 <?php foreach ($widgets as $widget): ?>
     <div class="uk-margin-medium uk-card uk-animation-slide-left <?= $widget->theme['card_style'] ?> uk-background-cover" <?= (!empty($widget->theme['card_image']) ? 'style="min-height:50vh" data-src="'. $widget->theme['card_image'] .'" uk-img':'') ?>>
       <div class="<?= $widget->theme['card_padding'] ?> uk-transition-toggle" tabindex="0">
